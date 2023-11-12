3336919f365e1e865421a260df0df4d5ddb59270
Sachin Somia <sachinsomia@gmail.com>
2023-11-12 01:41:17 +0000

    feat: Adds other store collys

--- main.go
+++ main.go
@@ -6,18 +6,73 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
+type ItemURL struct {
+	store string
+	url   string
+}
 
-func main() {
-	c := colly.NewCollector(colly.IgnoreRobotsTxt())
-	c.UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.360"
+type ItemLink struct {
+	name string
+	urls []ItemURL
+}
 
-  URL := "https://www.tesco.ie/groceries/en-IE/search?query=jack%20daniels"
+type Store string
 
-	c.OnRequest(func(r *colly.Request) {
+const (
+  tesco Store = "tesco"
+  supervalu Store = "supervalu"
+  dunnes Store = "dunnes"
+)
+
+var jd = ItemLink{
+	name: "Jack Daniel's",
+	urls: []ItemURL{
+		{store: "tesco", url: "https://www.tesco.ie/groceries/en-IE/search?query=jack%20daniel"},
+		{store: "supervalu", url: "https://shop.supervalu.ie/sm/delivery/rsid/5550/product/jack-daniels-old-no-7-whiskey-70-cl-id-1020340001"},
+    {store: "dunnes", url: "https://www.dunnesstoresgrocery.com/sm/delivery/rsid/258/product/jack-daniels-old-no-7-brand-tennessee-sour-mash-whiskey-70cl-id-100672192"},
+	},
+}
+
+var itemLinks = map[string]ItemLink{
+	"JD": jd,
+}
+
+func createCollyMap() map[string]*colly.Collector {
+  const USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+	supervaluColly := colly.NewCollector(colly.IgnoreRobotsTxt())
+  supervaluColly.UserAgent = USER_AGENT
+	supervaluColly.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+	supervaluColly.OnHTML(`[data-testid="pdpMainPrice-div-testId"]`, func(e *colly.HTMLElement) {
+    fmt.Printf("The Price of JD in Supervalu is: %v\n", e.Text[1:])
+	})
+
+  // NOTE: Dunnes currently does not work
+	dunnesColly := colly.NewCollector(colly.IgnoreRobotsTxt())
+  dunnesColly.UserAgent = USER_AGENT
+	dunnesColly.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+  dunnesColly.OnResponse(func (r *colly.Response) {
+    fmt.Printf("Dunne's Response:\n %v \n", r.Body)
+  })
+  dunnesColly.OnError(func(r *colly.Response, err error) {
+    fmt.Println(err.Error())
+  })
+	dunnesColly.OnHTML(`meta[itemprop="price"]`, func(e *colly.HTMLElement) {
+		fmt.Printf("The Price of JD in Supervalu is: %v\n", e.Attr("content"))
+	})
+
+	tescoColly := colly.NewCollector(colly.IgnoreRobotsTxt())
+	tescoColly.UserAgent = USER_AGENT
+
+
+	tescoColly.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 
-  c.OnHTML(`div[data-auto="product-tile"]`, func(e *colly.HTMLElement) {
+  tescoColly.OnHTML(`div[data-auto="product-tile"]`, func(e *colly.HTMLElement) {
     tile := e.DOM
     h3 := tile.Find("h3").Text()
     if h3 != "Jack Daniel's Old No. 7 Tennessee Whiskey 70 cL" {
@@ -32,5 +87,21 @@ func main() {
     fmt.Printf("Price of %v at Tesco is: %v \n", h3, price[1:])
 	})
 
-  c.Visit(URL)
+	return map[string]*colly.Collector{
+		"supervalu": supervaluColly,
+		"tesco":     tescoColly,
+		"dunnes":    dunnesColly,
+	}
+}
+
+func main() {
+  collys := createCollyMap()
+  for _, itemLink := range itemLinks {
+    for _, itemUrl := range itemLink.urls {
+      c, ok := collys[itemUrl.store]
+      if ok {
+        c.Visit(itemUrl.url)
+      }
+    }
+  }
 }
